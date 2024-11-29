1c68eeef622ab6cfbc7416235e3320c8b32f8f9a
matsumoto <dsongben@gmail.com>
2024-11-29 23:50:39 +0900

    next.jsをインストールしたフォルダ

---
+++ my-apps/src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "100px",
+      }}
+    >
+      <div>HELLO!!</div>
+      <div>HELLO!!</div>
+      <div>テスト</div>
+    </div>
+  );
+}
